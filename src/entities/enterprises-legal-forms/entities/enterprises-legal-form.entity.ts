29844f12227b82ad45e3f4bab67f9a2274d7002b
Diego Castillo <diegocashe17@gmail.com>
2022-04-15 15:12:12 -0400

    Underscored Tables name Changes

--- src/entities/enterprises-legal-forms/entities/enterprises-legal-form.entity.ts
+++ src/entities/enterprises-legal-forms/entities/enterprises-legal-form.entity.ts
@@ -1,15 +1,17 @@
 import { Column, Model, Table, HasMany, Unique } from 'sequelize-typescript';
 import { Enterprise } from "src/entities/enterprises/entities/enterprise.entity";
 
-@Table
+@Table({
+    tableName: 'enterprises_legal_forms'
+})
 export class EnterprisesLegalForm extends Model {
     @Unique
     @Column
     name: string;
-  
+
     @Column
     description: string;
-  
+
     @HasMany(() => Enterprise)
     enterprises: Enterprise[]
 }
